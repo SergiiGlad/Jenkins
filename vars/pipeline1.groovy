d97aa00c48abf2b3536b0168821cb909eef21164
sergiiglad <sergii.gladchenko@gmail.com>
2020-01-06 12:01:29 +0200

    Update pipeline1.groovy

--- vars/pipeline1.groovy
+++ vars/pipeline1.groovy
@@ -1,4 +1,4 @@
 #!groovy
 
 
-apply from: 'https://raw.githubusercontent.com/SergiiGlad/Jenkins/master/Jenkinsfile.1?token=${env.GITHUB_TOKEN}'
+apply from: 'https://raw.githubusercontent.com/SergiiGlad/Jenkins/master/Jenkinsfile?token=${env.GITHUB_TOKEN}'
