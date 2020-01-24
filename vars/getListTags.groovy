0d436cf45a4b6c98232a26a2ff80bb6e4ae90831
Sergii Gladchenko <sgladc@macmini576.ddns.softservecom.com>
2020-01-24 14:03:34 +0200

    call

--- vars/getListTags.groovy
+++ vars/getListTags.groovy
@@ -1,11 +1,12 @@
 #!groovy
+def call() {
+  def gettags
 
-def gettags
+  sshagent(credentials: ['git_ssh']) {
+     env.GIT_SSH_COMMAND = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+     gettags = sh(returnStdout: true, script: 'git ls-remote -t ssh://git@github.com/sergiiglad/web.git')
+  }
 
-sshagent(credentials: ['git_ssh']) {
-   env.GIT_SSH_COMMAND = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-   gettags = sh(returnStdout: true, script: 'git ls-remote -t ssh://git@github.com/sergiiglad/web.git')
+  return gettags.readLines()
+         .collect { it.split()[1].replaceAll('refs/tags/', '') }
 }
-
-return gettags.readLines()
-       .collect { it.split()[1].replaceAll('refs/tags/', '') }
