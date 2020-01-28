4d88ed79d35bd91c5b0897d0309dfbe344d4c46e
Sergii Gladchenko <sgladc@macmini576.ddns.softservecom.com>
2020-01-28 18:42:38 +0200

    kaniko

---
+++ vars/kanikoTemplate.groovy
@@ -0,0 +1,18 @@
+def call(String podLabel, Closure code) { podTemplate(
+    cloud: 'kubernetes',
+    namespace: 'jenkins',
+    label: podLabel,
+    containers: [
+      containerTemplate(
+        name: 'golang',
+        image: 'golang:1.13.0-alpine',
+        ttyEnabled: true,
+        command: 'cat'),
+      containerTemplate(
+        name: 'kaniko',
+        image: 'gcr.io/kaniko-project/executor:latest',
+        ttyEnabled: true,
+        command: 'cat')
+    ]) {
+code() }
+}
