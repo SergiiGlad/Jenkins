e91e7b82c8076c7d5921bae86022e339e3967911
Sergii Gladchenko <sgladc@macmini576.ddns.softservecom.com>
2020-01-29 18:08:13 +0200

    regcred

--- vars/kanikoTemplate.groovy
+++ vars/kanikoTemplate.groovy
@@ -14,7 +14,7 @@ def call(String podLabel, Closure code) { podTemplate(
         ttyEnabled: true,
         command: '/busybox/cat')],
     volumes: [
-      secretVolume(mountPath: '/kaniko/.docker', secretName: 'dockercred')
+      secretVolume(mountPath: '/kaniko/.docker', secretName: 'regcred')
     ]) {    
 code() }
 }
