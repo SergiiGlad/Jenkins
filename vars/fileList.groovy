c367eff73c16e1834f0a3eea820f752c84972818
Sergii Gladchenko <sgladc@macmini576.ddns.softservecom.com>
2020-02-05 15:18:52 +0200

    collect

--- vars/fileList.groovy
+++ vars/fileList.groovy
@@ -2,12 +2,8 @@ def call(String path){
 
 def findList=sh(returnStdout: true, script: "find $path -name *.yaml")
 
-def filePathList = []
 
-findList.each {
-   println it
-   filePathList.add( it )
-}
-return filePathList
+return findList.readLines()
+         .collect( it.sudstring(path.length()+1) )
    
 }
