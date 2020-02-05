44f84ef594d99b59fe4aa7f31c9153d775fa8c69
Sergii Gladchenko <sgladc@macmini576.ddns.softservecom.com>
2020-02-05 15:10:37 +0200

    it

--- vars/fileList.groovy
+++ vars/fileList.groovy
@@ -5,8 +5,8 @@ def findList=sh(returnStdout: true, script: "find $path -name *.yaml")
 def filePathList = []
 
 findList.each {
-   println it.substring(2)
-   filePathList.add( it.substring( 2 ) )
+   println it
+   filePathList.add( it )
 }
 return filePathList
    
