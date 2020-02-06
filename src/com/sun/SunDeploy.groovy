213610146949642f32d5348830594aadc06e2505
Sergii Gladchenko <sgladc@macmini576.ddns.softservecom.com>
2020-02-06 17:43:51 +0200

    SunDeploy

---
+++ src/com/sun/SunDeploy.groovy
@@ -0,0 +1,16 @@
+package com.sun
+
+class SunDeploy {
+    String nameSpace
+    String releaseName
+    
+    SunDeploy(String yamFilePath) {
+        this.nameSpace = yamlFilePath.split('/')[0]
+        this.releaseName = yamlFilePath.split('/')[1].split(/\./)[0]
+    }
+
+    def print() {
+        println("k8s_namespace: "+this.nameSpace+
+        "\nhelm_release_name: "+this.releaseName)
+    }
+}
