f6fb7ee5c108eff380ccaf68af9b86e75eb08cc7
Sergii Gladchenko <sgladc@macmini576.ddns.softservecom.com>
2020-02-14 17:38:10 +0200

    bad tag

--- vars/isExistsGitTags.groovy
+++ vars/isExistsGitTags.groovy
@@ -1,9 +1,6 @@
 boolean call(tag) {
-    
-    env.GIT_SSH_COMMAND = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-    def gettags = sh(returnStdout: true, script: 'git ls-remote -t ssh://git@github.com/sergiiglad/web.git')
    
-    return tag in gettags.readLines()
-            .collect { it.split()[1].replaceAll('refs/tags/', '') }   
+    return tag in sh(returnStdout: true, script: 'git ls-remote -t https://github.com/sergiiglad/web.git')
+        .readLines().collect { it.split()[1].replaceAll('refs/tags/', '') }   
 
 }
